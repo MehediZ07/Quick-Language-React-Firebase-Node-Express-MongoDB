e16ae4aaf4fd559d14c3423943b11d5c1fb95790
Mehedi Zaman <mehedi.zaman.professional@gmail.com>
2025-02-12 13:02:50 +0600

    Quick Question & Answer part edit

--- src/components/QuestionAndAnser.jsx
+++ src/components/QuestionAndAnser.jsx
@@ -4,14 +4,14 @@ export default function QuestionAndAnser() {
       <h1 className="text-3xl font-bold text-center text-gray-500 my-10">
         Quick Question & Answer
       </h1>
-      <div className="join join-vertical max-w-7xl w-full ">
+      <div className="join join-vertical max-w-7xl w-full rounded-lg">
         {/* Question 1 */}
-        <div className="collapse collapse-arrow join-item border-base-300 border">
+        <div className="collapse collapse-arrow join-item ">
           <input type="radio" name="faq-accordion" defaultChecked />
           <div className="collapse-title text-xl font-medium">
             What languages can I learn on this platform?
           </div>
-          <div className="collapse-content">
+          <div className="collapse-content border-b-4 solid border-[#acea80c7] rounded-xl rounded- ">
             <p>
               You can learn a wide variety of languages, including Spanish,
               French, Mandarin, German, Japanese, and more. We offer over 50
@@ -21,12 +21,12 @@ export default function QuestionAndAnser() {
         </div>
 
         {/* Question 2 */}
-        <div className="collapse collapse-arrow join-item border-base-300 border">
+        <div className="collapse collapse-arrow join-item ">
           <input type="radio" name="faq-accordion" />
           <div className="collapse-title text-xl font-medium">
             Are the lessons conducted by native speakers?
           </div>
-          <div className="collapse-content">
+          <div className="collapse-content border-b-4 solid border-[#acea80c7] rounded-xl rounded- ">
             <p>
               Yes, most of our tutors are native speakers with professional
               teaching experience, ensuring an authentic and immersive learning
@@ -36,12 +36,12 @@ export default function QuestionAndAnser() {
         </div>
 
         {/* Question 3 */}
-        <div className="collapse collapse-arrow join-item border-base-300 border">
+        <div className="collapse collapse-arrow join-item ">
           <input type="radio" name="faq-accordion" />
           <div className="collapse-title text-xl font-medium">
             Can I take lessons on my own schedule?
           </div>
-          <div className="collapse-content">
+          <div className="collapse-content border-b-4 solid border-[#acea80c7] rounded-xl rounded- ">
             <p>
               Absolutely! Our platform allows you to schedule lessons at times
               that work best for you, making language learning flexible and
@@ -51,12 +51,12 @@ export default function QuestionAndAnser() {
         </div>
 
         {/* Question 4 */}
-        <div className="collapse collapse-arrow join-item border-base-300 border">
+        <div className="collapse collapse-arrow join-item ">
           <input type="radio" name="faq-accordion" />
           <div className="collapse-title text-xl font-medium">
             Is there a free trial available?
           </div>
-          <div className="collapse-content">
+          <div className="collapse-content border-b-4 solid border-[#acea80c7] rounded-xl rounded- ">
             <p>
               Yes, we offer a free trial lesson for new users so you can explore
               the platform and see if it’s the right fit for your learning
@@ -66,12 +66,12 @@ export default function QuestionAndAnser() {
         </div>
 
         {/* Question 5 */}
-        <div className="collapse collapse-arrow join-item border-base-300 border">
+        <div className="collapse collapse-arrow join-item ">
           <input type="radio" name="faq-accordion" />
           <div className="collapse-title text-xl font-medium">
             What is the cost of the lessons?
           </div>
-          <div className="collapse-content">
+          <div className="collapse-content border-b-4 solid border-[#acea80c7] rounded-xl">
             <p>
               The cost of lessons varies depending on the tutor and the length
               of the session. We offer competitive pricing starting at just $10
